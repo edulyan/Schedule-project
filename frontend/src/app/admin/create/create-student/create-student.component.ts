2d0571abd8ab4bf8c00b78551cc93b056ea779f1
edulyan <mnatik94@mail.ru>
2022-04-10 18:11:56 +0300

    updated.create-student-table

--- frontend/src/app/admin/create/create-student/create-student.component.ts
+++ frontend/src/app/admin/create/create-student/create-student.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+export interface PeriodicElement {
+  id: number;
+  firstname: string;
+  lastname: string;
+  password: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { id: 1, firstname: 'Hydrogen', lastname: '', password: '123' },
+  { id: 2, firstname: 'Helium', lastname: '', password: '123' },
+  { id: 3, firstname: 'Lithium', lastname: '', password: '123' },
+  { id: 4, firstname: 'Beryllium', lastname: '', password: '123' },
+  { id: 5, firstname: 'Boron', lastname: '', password: '123' },
+  { id: 6, firstname: 'Carbon', lastname: '', password: '123' },
+  { id: 7, firstname: 'Nitrogen', lastname: '', password: '123' },
+  { id: 8, firstname: 'Oxygen', lastname: '', password: '123' },
+  { id: 9, firstname: 'Fluorine', lastname: '', password: '123' },
+  { id: 10, firstname: 'Neon', lastname: '', password: '123' },
+];
 
 @Component({
   selector: 'app-create-student',
   templateUrl: './create-student.component.html',
-  styleUrls: ['./create-student.component.scss']
+  styleUrls: ['./create-student.component.scss'],
 })
 export class CreateStudentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  displayedColumns: string[] = [
+    'ID',
+    'firstname',
+    'lastname',
+    'password',
+    'email',
+    'phone',
+    'group_title',
+  ];
+  dataSource = ELEMENT_DATA;
 }
--- frontend/src/app/admin/create/create-student/create-student.module.ts
+++ frontend/src/app/admin/create/create-student/create-student.module.ts
@@ -2,14 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { CreateStudentComponent } from './create-student.component';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   exports: [CreateStudentComponent],
   declarations: [CreateStudentComponent],
-  imports: [CommonModule, FormsModule, BrowserModule, AppRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+  ],
   providers: [],
 })
 export class CreateStudentModule {}
